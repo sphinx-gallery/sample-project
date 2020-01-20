fd1d9ac3b78ba0d30329a3438df52d24a1cfd187
Lucy Liu <jliu176@gmail.com>
2020-01-20 17:43:59 +0100

    amend data_dataload fun

--- sample-gallery-1/plot_1_download_data.py
+++ sample-gallery-1/plot_1_download_data.py
@@ -5,22 +5,25 @@ Data download example
 This example shows one way of dealing with large data files required for your
 examples.
 
-Data is downloaded, by default to the folder ``~/sg_data``, and the location
-is stored in a config file under the key 'data_path'. You can now use the data
-again in a different example without downloading the data again.
+The ``download_data`` function first checks if the data has already been
+downloaded, looking in either the data directory saved the configuration file
+(by default ``~/.sg_template``) or the default data directory. If the data has
+not already been downloaded, it downloads the data from the url and saves the
+data directory to the configuration file. This allows you to use the data
+again in a different example without downloading it again.
 
-Note that examples in the gallery are ordered according to their filenames,
-thus the number after 'plot\_' dictates the order the example appears in the
-gallery.
+Note that examples in the gallery are ordered according to their filenames, thus
+the number after 'plot\_' dictates the order the example appears in the gallery.
 """
 
-import SampleModule.data_download as dd
 import pandas as pd
+import SampleModule.data_download as dd
 
 
 data_file = dd.download_data(
     url='http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',\
-    data_file_name='iris.csv')
+    data_file_name='iris.csv',
+    data_key='data_dir')
 
 iris = pd.read_csv(data_file, header=None)
 iris.head()
